96e130a943ca2a552388771f8dbe26a78b19ea53
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2023-03-20 13:25:10 +0900

    feat: add lexical model test case

--- domain/src/model/common/rte/editor_state.rs
+++ domain/src/model/common/rte/editor_state.rs
@@ -23,3 +23,77 @@ impl Into<serde_json::Value> for &RteEditorState {
         serde_json::to_value(self).unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialize() {
+        let serialized = r#"{
+  "root": {
+    "children": [
+      {
+        "type": "list",
+        "children": [
+          {
+            "type": "listitem",
+            "children": [
+              {
+                "type": "text",
+                "detail": 0,
+                "format": 16,
+                "mode": "normal",
+                "style": "",
+                "text": "aaa",
+                "version": 1
+              }
+            ],
+            "direction": "ltr",
+            "format": "",
+            "indent": 0,
+            "version": 1,
+            "value": 1
+          }
+        ],
+        "direction": "ltr",
+        "format": "",
+        "indent": 0,
+        "version": 1,
+        "listType": "bullet",
+        "start": 1,
+        "tag": "ul"
+      },
+      {
+        "type": "code",
+        "children": [
+          {
+            "type": "code-highlight",
+            "detail": 0,
+            "format": 0,
+            "mode": "normal",
+            "style": "",
+            "text": "a",
+            "version": 1
+          }
+        ],
+        "direction": "ltr",
+        "format": "",
+        "indent": 0,
+        "version": 1,
+        "language": "plain"
+      }
+    ],
+    "direction": "ltr",
+    "format": "",
+    "indent": 0,
+    "type": "root",
+    "version": 1
+  }
+}"#;
+        let json_value: serde_json::Value = serde_json::from_str(serialized).unwrap();
+        let deserialized: RteEditorState = serde_json::from_str(serialized).unwrap();
+        let re_serialized: serde_json::Value = deserialized.into();
+        assert_eq!(json_value, re_serialized);
+    }
+}
