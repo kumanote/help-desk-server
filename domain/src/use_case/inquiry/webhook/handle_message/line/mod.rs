0e0b2d5d56116349c55f8897314867739c0716d9
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2023-03-29 13:26:22 +0900

    feat: remove line member join/leave event handler

--- domain/src/use_case/inquiry/webhook/handle_message/line/mod.rs
+++ domain/src/use_case/inquiry/webhook/handle_message/line/mod.rs
@@ -1,8 +1,6 @@
 mod follow_event;
 mod join_event;
 mod leave_event;
-mod member_join_event;
-mod member_leave_event;
 mod message_event;
 mod un_follow_event;
 mod unsent_event;
@@ -10,8 +8,6 @@ mod unsent_event;
 pub use follow_event::*;
 pub use join_event::*;
 pub use leave_event::*;
-pub use member_join_event::*;
-pub use member_leave_event::*;
 pub use message_event::*;
 pub use un_follow_event::*;
 pub use unsent_event::*;
